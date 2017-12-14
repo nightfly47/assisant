6d3f2d89c228353f771eb206603a02c96204b903
dogcode <lostwater0820@gmail.com>
2017-12-14 15:58:32 +0800

    Revert "api ok"
    This reverts commit 12362e1da5dd9c7a648595b7619d1a4d199a1f9b.

--- admin_ass/urls.py
+++ admin_ass/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from admin_ass import views
-from admin_ass import query
 
 urlpatterns = [
     path('login_admin/', views.login_admin),
     path('admin_home/', views.admin_home),
-    path('role/', query.role_query),
-    path('summon/', query.summon_query),
-    path('boss/', query.boss_query),
-    path('config/', query.config_query),
+    path('get_roles/', views.get_roles),
+    path('post_role/', views.post_role),
 ]
